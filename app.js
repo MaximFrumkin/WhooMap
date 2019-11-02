4516820eb714fcf91b42186b418833d110b05545
kobecow <sneeiwji@gmail.com>
2019-11-02 18:35:08 -0400

    type : clean files
    trash some files

---
+++ app.js
@@ -0,0 +1,62 @@
+var fs = require('fs');
+var express = require('express');
+var app = express();
+
+app.use(express.static('public'));
+
+// This responds with "Hello World" on the homepage
+app.get('/', function (req, res) {
+    console.log("Got a GET request for the homepage");
+    res.send('Hello GET');
+ })
+
+ var server = app.listen(3000, function () {
+    var host = server.address().address
+    var port = server.address().port
+ 
+    console.log("Example app listening at http://%s:%s", host, port)
+ })
+
+
+
+
+
+
+
+
+/*
+
+fs.readFile('public/index.html', function (err, html) {
+    if (err) {
+        throw err; 
+    }       
+    http.createServer(function(request, response) {  
+        response.writeHeader(200, {"Content-Type": "text/html"});  
+        response.write(html);  
+        response.end();  
+    }).listen(3000);
+});
+
+
+let rawdata = fs.readFileSync('public/gpr_000b11a_e.json');
+let geojson_parsed = JSON.parse(rawdata);
+
+app.get('/map_data', function (req, res) {
+    log('This is my text', function (err) {
+      if (err) throw err;
+      res.send(geojson_parsed);
+    });
+  });
+
+
+
+app.get('/save', function (req, res) {
+  fs.writeFile('log.txt', 'This is my text', function (err) {
+    if (err) throw err;
+    console.log('Replaced!');
+    res.send('Replaced!')
+  });
+})
+
+app.listen(3000, () => console.log('Example app listening on port 3000!'))
+*/
