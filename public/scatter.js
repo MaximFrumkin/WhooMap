4f7dae654b9d6e938bca12d8e7e08b4348841e15
Maxim Frumkin <maximfru@gmail.com>
2019-11-02 22:04:11 -0400

    fixed scatter plot for displaying altitude and concentration data

---
+++ public/scatter.js
@@ -0,0 +1,59 @@
+var Ozone = {
+    x: [1, 2, 3, 4],
+    y: [10, 15, 13, 17],
+    mode: 'markers',
+    name: 'Ozone',
+    marker: { size: 12 }
+  };
+  
+  var Methane = {
+    x: [2, 3, 4, 5],
+    y: [16, 5, 11, 10],
+    mode: 'markers',
+    name: 'Methane',
+    marker: { size: 12 }
+  };
+  
+  var CarbonDioxide = {
+    x: [1, 2, 3, 4],
+    y: [12, 9, 15, 12],
+    mode: 'markers',
+    name: 'Carbon Dioxide',
+    marker: { size: 12 }
+  };
+  
+  var data = [ Ozone, Methane, CarbonDioxide ];
+  
+  var layout = {
+    title: {
+      text:'Concentration vs Altitude, Year: XXXX, Latitude: XXXX, Longitude: XXXX',
+      font: {
+        family: 'Courier New, monospace',
+        size: 24
+      },
+      xref: 'paper',
+      x: 0.05,
+    },
+    xaxis: {
+      title: {
+        text: 'Altitude (units)',
+        font: {
+          family: 'Courier New, monospace',
+          size: 18,
+          color: '#7f7f7f'
+        }
+      },
+    },
+    yaxis: {
+      title: {
+        text: 'Concentration (units)',
+        font: {
+          family: 'Courier New, monospace',
+          size: 18,
+          color: '#7f7f7f'
+        }
+      }
+    }
+  };
+  
+  Plotly.newPlot('myDiv', data, layout, {showSendToCloud: true});
