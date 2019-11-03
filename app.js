0cf97aeabd0279f5d7c924859228a4f5501fad88
Shuang Liang <shuangliang117@gmail.com>
2019-11-03 16:30:52 -0500

    fix some more syntax bugs :)

--- app.js
+++ app.js
@@ -16,16 +16,16 @@ app.get('/', function (req, res) {
    console.log(req.query);
   
   
-})
+});
 
 
 
  var server = app.listen(3000, function () {
-    var host = server.address().address
-    var port = server.address().port
+    var host = server.address().address;
+    var port = server.address().port;
  
-    console.log("Example app listening at http://%s:%s", host, port)
- })
+    console.log("Example app listening at http://%s:%s", host, port);
+ });
 
 
 
