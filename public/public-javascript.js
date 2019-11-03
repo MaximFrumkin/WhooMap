18ed1d499887c9bd9c7964b463023cc0ea9b3b28
kobecow <sneeiwji@gmail.com>
2019-11-02 22:05:53 -0400

    nothing to share

--- public/public-javascript.js
+++ public/public-javascript.js
@@ -1,3 +1,6 @@
+
+
+
 //--------- Creating array from json
 var addressPoints = [];
 for (var i in myData) {
@@ -11,99 +14,100 @@ for (var i in myData) {
     ]);
 }
 var currentYear = 2004;
-        var subArray = [];
-        var map;
-        var timer;
-        var lgroup;
-        var heat;
-        jQuery(document).ready(function () {
-            jQuery(".gas").change(function (e) {
-                //debugger;
-                //addressPoints.filter(gases,e.data);
-                //addressPoints.filter(point => point[1] == e.data);
-            });
-            map = L.map('map', {
-                center: [56.1304, -108.3468],
-                zoom: 3,
-                continuousWorld: true
-            });  //.setView([56.1304, -108.3468], 5);
-            var tiles = L.tileLayer('http://{s}.tile.osm.org/{z}/{x}/{y}.png', {
-                attribution: '&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors',
-            }).addTo(map);
-            subArray = addressPoints.filter(point => point[3] == 'O3');
-            heat = L.heatLayer(subArray, {
-                "id": 'layer1',
-                "gradient": { 0.4: 'yellow', 0.5: 'red', .6: 'blue' },
-                "radius": 70,
-                "blur": 10,
-            });//.addTo(map)
-            map.addLayer(heat);
-            //var animationControl = L.control.animation('control',{
-            //    animationControl: true,
-            //    playbackSpeed: 1000,
-            //    playbackSpeeds: [100, 500, 1000, 10000, 100000],
-            //    stepWidth: 1000
-            //}).addTo(map);
+var subArray = [];
+var map;
+var timer;
+var lgroup;
+var heat;
 
-        });
-        function playMap() {
-            timer = setInterval(callMap, 1000);
-        }
-        function callMap() {
-            debugger;
-            jQuery('#year').text(currentYear);
-            createMap('O3', currentYear); //jQuery(".gas input[type='radio']:checked").val()
-            currentYear++;
-            if (currentYear >= 2020) {
-                //window.clearInterval(timer);
-                currentYear = 2004;
-            }
-        }
+jQuery(document).ready(function () {
+    jQuery(".gas").change(function (e) {
+        //debugger;
+        //addressPoints.filter(gases,e.data);
+        //addressPoints.filter(point => point[1] == e.data);
+    });
+    map = L.map('map', {
+        center: [56.1304, -108.3468],
+        zoom: 3,
+        continuousWorld: true
+    });  //.setView([56.1304, -108.3468], 5);
+    var tiles = L.tileLayer('http://{s}.tile.osm.org/{z}/{x}/{y}.png', {
+        attribution: '&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors',
+    }).addTo(map);
+    subArray = addressPoints.filter(point => point[3] == 'O3');
+    heat = L.heatLayer(subArray, {
+        "id": 'layer1',
+        "gradient": { 0.4: 'yellow', 0.5: 'red', .6: 'blue' },
+        "radius": 70,
+        "blur": 10,
+    });//.addTo(map)
+    map.addLayer(heat);
+    //var animationControl = L.control.animation('control',{
+    //    animationControl: true,
+    //    playbackSpeed: 1000,
+    //    playbackSpeeds: [100, 500, 1000, 10000, 100000],
+    //    stepWidth: 1000
+    //}).addTo(map);
 
-        function createMap(gas, year) {
-            debugger;
-            if ((year !== null && year !== undefined) && year !== '') {
-                subArray = addressPoints.filter(point => point[3] == gas && point[4] == year);
-            } else {
-                subArray = addressPoints.filter(point => point[3] == gas);
-            }
-            //--- Remove existing Map if exits
-            if (typeof map !== 'undefined') {
-                map.removeLayer(heat);
-            }
-            heat = L.heatLayer(subArray, {
-                "id": 'layer1',
-                "gradient": { 0.4: 'yellow', 0.5: 'red', .6: 'blue' },
-                "radius": 70,
-                "blur": 10,
-            });//.addTo(map)
-            map.addLayer(heat);
+    map.addLayer(heat);
 
-        }
-        function addLayer(gas, year) {
-            if (year !== null && year !== '') {
-                subArray = addressPoints.filter(point => point[3] == gas && point[4] == year);
-            } else {
-                subArray = addressPoints.filter(point => point[3] == gas);
-            }
-            //--- Remove existing Map if exits
-            if (typeof map !== 'undefined') {
-                debugger;
-                map.remove(heat);
-            }
-            heat = L.heatLayer(subArray, {
-                "id": 'layer1',
-                "gradient": { 0.4: 'yellow', 0.5: 'red', .6: 'blue' },
-                "radius": 70,
-                "blur": 10,
-            });//.addTo(map)
-            map.addLayer(heat);
-        }
+    function onMapClick(e) {
+        console.log("You clicked the map at " + e.latlng);
+        var marker = L.marker(e.latlng).addTo(map);
+    
+    }
+    map.on('click', onMapClick);
 
-        console.log("TEST1");
+});
+function playMap() {
+    timer = setInterval(callMap, 1000);
+}
+function callMap() {
+    debugger;
+    jQuery('#year').text(currentYear);
+    createMap('O3', currentYear); //jQuery(".gas input[type='radio']:checked").val()
+    currentYear++;
+    if (currentYear >= 2020) {
+        //window.clearInterval(timer);
+        currentYear = 2004;
+    }
+}
 
-        var marker = L.marker([10.496093,-66.881935]).addTo(map).on('mouseover', onClick);
-        console.log("TEST2");
-        function onClick(e) {
-            console.log(this.getLatLng());
-        }
+function createMap(gas, year) {
+    debugger;
+    if ((year !== null && year !== undefined) && year !== '') {
+        subArray = addressPoints.filter(point => point[3] == gas && point[4] == year);
+    } else {
+        subArray = addressPoints.filter(point => point[3] == gas);
+    }
+    //--- Remove existing Map if exits
+    if (typeof map !== 'undefined') {
+        map.removeLayer(heat);
+    }
+    heat = L.heatLayer(subArray, {
+        "id": 'layer1',
+        "gradient": { 0.4: 'yellow', 0.5: 'red', .6: 'blue' },
+        "radius": 70,
+        "blur": 10,
+    });//.addTo(map)
+}
+function addLayer(gas, year) {
+    if (year !== null && year !== '') {
+        subArray = addressPoints.filter(point => point[3] == gas && point[4] == year);
+    } else {
+        subArray = addressPoints.filter(point => point[3] == gas);
+    }
+    //--- Remove existing Map if exits
+    if (typeof map !== 'undefined') {
+        debugger;
+        map.remove(heat);
+    }
+    heat = L.heatLayer(subArray, {
+        "id": 'layer1',
+        "gradient": { 0.4: 'yellow', 0.5: 'red', .6: 'blue' },
+        "radius": 70,
+        "blur": 10,
+    });//.addTo(map)
+    map.addLayer(heat);
+}
+
